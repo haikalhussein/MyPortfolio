ae7595d9b0b799ac7c814efe2658e52f56aa787e
haikalhussein <mhaikal.mhussein@gmail.com>
2018-10-25 00:30:17 +0800

    Updated portfolio to include attr for position.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180802152715) do
+ActiveRecord::Schema.define(version: 20181024162139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180802152715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "position"
     t.index ["slug"], name: "index_portfolios_on_slug", unique: true
   end
 
