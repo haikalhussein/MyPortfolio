2a97ca55caca09cf4cfa0a7087cee203533b65ae
haikalhussein <mhaikal.mhussein@gmail.com>
2019-10-12 01:22:34 +0800

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://haikalhussein-resume.herokuapp.com', 'http://haikalhussein-resume.herokuapp.com']
+  config.action_cable.url = "wss://haikalhussein-resume.herokuapp.com/cable"
 end
